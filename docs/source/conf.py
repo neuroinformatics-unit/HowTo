f9304eddfb82a02735bb667cc5c2d65e2fe8b635
lauraporta <29216006+lauraporta@users.noreply.github.com>
2022-11-29 16:52:11 +0000

    Add logo and favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,3 +108,5 @@ html_baseurl = 'https://troubleshooting.neuroinformatics.dev/'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+html_logo = "logo.png"
+html_favicon = "logo.png"
